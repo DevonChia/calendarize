afe43b3d45f52f86c6e8571bd5e7a60fc0e72717
Devon Chia <chia.devon@gmail.com>
2025-07-26 13:55:31 +0800

    init app setup

---
+++ src/main/java/com/calendarize/calendarize/CalendarizeApplication.java
@@ -0,0 +1,13 @@
+package com.calendarize.calendarize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendarizeApplication.class, args);
+	}
+
+}
